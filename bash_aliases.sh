1207688048b59ec4affa67cb582e768c728607ff
ali <samzhab@gmail.com>
2020-12-14 21:51:38 -0800

    added bash_aliases.sh

---
+++ bash_aliases.sh
@@ -0,0 +1,12 @@
+alias rv='rvm'
+alias open='xdg-open .'
+alias nopt='xinput --disable 15'
+alias pt='xinput --enable 15'
+alias gcmt='git commit'
+alias gpsh='git push'
+alias gpll='git pull'
+alias gadd='git add *'
+alias rvmaws=alias rvmaws='rvm 2.7.0@aws-api' && cd ~/workspace/aws-doc-sdk-examples/ruby/example_code && atom .
+
+to add - alias startup='pwbm
+
